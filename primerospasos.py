114d03d96e89a872d59c8f8ae598d15c078cc1d2
Aman21 <112637273+AmanDLVS@users.noreply.github.com>
2024-01-10 11:14:36 +0100

    Create primerospasos.py

---
+++ primerospasos.py
@@ -0,0 +1,34 @@
+NOMBRE = 'AMAN'
+edad = 21
+print(NOMBRE)
+print(edad)
+
+print("Introduce tu apellido: ")
+apellido = input("Introduce el apellido: ")
+print("|----------------|")
+for i in range(10): 
+    for j in range(5, -1,-2): 
+        print("*"*(i+1))
+
+array = {1,2,3,4}
+print(array)
+patata_tupla = {"asdas",23,'asdasd',edad+edad}
+print(patata_tupla)
+patata_lista = ["asdas",23,'asdasd',edad+edad]
+print(patata_lista)
+patata_lista[0] = 23
+print(patata_lista)
+dicci_paises = {
+    "España" : "Madrid",
+    "Francia" : "Paris",
+    "Italia" : "Roma"
+}
+print(dicci_paises["España"])
+patata = 10
+while(patata>2):
+    print(patata)
+    patata=patata-1
+    
+for i in range(10): 
+    for j in range(5, -1,-2): 
+        print("*"*(i+1))
